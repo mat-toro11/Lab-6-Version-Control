0088ff87103e53efa126f9fb9b54a1825283621c
Kriti <kritishah989@gmail.com>
2023-10-28 23:41:22 -0400

    new decode from kriti

--- lab6.py
+++ lab6.py
@@ -1,5 +1,29 @@
 
 # Kriti Shah and Matthew Toro
+def encoder(password): #Encoder function, returns encoded password
+    x = []
+    y = ''
+
+    for i in range(0, len(password)):
+        x.append(int(password[i])+3)
+
+    for i in x:
+        y += str(i)
+
+    return y
+
+
+def decoder(password):  # Encoder function, adding three
+    decoded_password = ''
+
+    for digit in password:  # all the digit in password
+
+        if digit.isdigit():  # if the digits are a digit
+            number = int(digit)  # the digit makes it into an integer = number
+            decode = number - 3  # make the encode by adding three to the digits
+            decoded_password += str(decode)  # turn each decoded digit to a string and add to the new password str
+
+    return decoded_password  # return the new string
 
 def main():
     condition = True
@@ -15,37 +39,13 @@ def main():
             print()
 
         if user_input == 2: #Decoder
-            print('The encoded password is ' + str(encoded) + ', and the original password is ' + str(decoded(encoded)) + '.')
+            print('The encoded password is ' + str(encoded) + ', and the original password is ' + str(decoder(encoded)) + '.')
             print()
 
         if user_input == 3: #Exit function
             exit()
 
-
-def encoder(password): #Encoder function, returns encoded password
-    x = []
-    y = ''
-
-    for i in range(0, len(password)):
-        x.append(int(password[i])+3)
-
-    for i in x:
-        y += str(i)
-
-    return y
-
-def decoded(password): #Decoder function, inverse of encoder
-    x = []
-    y = ''
-
-    for i in range(0, len(password)):
-        x.append(int(password[i])-3)
-
-    for i in x:
-        y += str(i)
-
-    return y
-
-
 if __name__ == '__main__': #Main
     main()
+
+
